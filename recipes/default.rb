523394fe519b73d69a52b5f076b776125fadd0d0
Pratheep Premaraj <PPremaraj@spartaglobal.com>
2019-08-19 10:45:49 +0100

    installing

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,10 +3,26 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+apt_update 'update_sources' do
+  action :update
+end
+
 package 'python' do
   action :install
 end
 
-package 'python_pip' do
-  action: install
+package 'python-pip' do
+  action :install
+end
+
+execute 'python-pip' do
+  install: Flask==0.10.1
+  install: Jinja2==2.7.3
+  install: MarkupSafe==0.23
+  install: Werkzeug==0.9.6
+  install: gnureadline==6.3.3
+  install: itsdangerous==0.24
+  install: rauth==0.7.0
+  install: requests==2.3.0
+  install: wsgiref==0.1.2
 end
