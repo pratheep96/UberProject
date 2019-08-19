66a01a99354dd757d395ea0384e078871eef703c
Pratheep Premaraj <PPremaraj@spartaglobal.com>
2019-08-19 11:24:14 +0100

    plugins installed

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,13 +16,24 @@ package 'python-pip' do
 end
 
 execute 'python-pip' do
-  install: Flask==0.10.1
-  install: Jinja2==2.7.3
-  install: MarkupSafe==0.23
-  install: Werkzeug==0.9.6
-  install: gnureadline==6.3.3
-  install: itsdangerous==0.24
-  install: rauth==0.7.0
-  install: requests==2.3.0
-  install: wsgiref==0.1.2
+  command 'pip install Flask==0.10.1'
+  command 'pip install Jinja2==2.7.3'
+  command 'pip install MarkupSafe==0.23'
+  command 'pip install Werkzeug==0.9.6'
+  command 'pip install: Gnureadline==6.3.3'
+  command 'pip install: Itsdangerous==0.24'
+  command 'pip install: Rauth==0.7.0'
+  command 'pip install: Requests==2.3.0'
+  command 'pip install: Wsgiref==0.1.2'
 end
+
+
+  # install: Flask==0.10.1
+  # install: Jinja2==2.7.3
+  # install: MarkupSafe==0.23
+  # install: Werkzeug==0.9.6
+  # install: Gnureadline==6.3.3
+  # install: Itsdangerous==0.24
+  # install: Rauth==0.7.0
+  # install: Requests==2.3.0
+  # install: Wsgiref==0.1.2
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -15,15 +15,18 @@ describe 'python_uber::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should install plugins' do
+      expect(chef_run).to run_execute 'pip install flask=0.10.1'
+      expect(chef_run).to run_execute 'pip install Jinja2==2.7.3'
+      expect(chef_run).to run_execute 'pip install MarkupSafe==0.23'
+      expect(chef_run).to run_execute 'pip install Werkzeug==0.9.6'
+      expect(chef_run).to run_execute 'pip install: Gnureadline==6.3.3'
+      expect(chef_run).to run_execute 'pip install: Itsdangerous==0.24'
+      expect(chef_run).to run_execute 'pip install: Rauth==0.7.0'
+      expect(chef_run).to run_execute 'pip install: Requests==2.3.0'
+      expect(chef_run).to run_execute 'pip install: Wsgiref==0.1.2'
     end
+
   end
 end
