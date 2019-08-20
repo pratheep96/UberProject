7bf7cfe6877d621fbe22991f6da74174639b265e
Pratheep Premaraj <PPremaraj@spartaglobal.com>
2019-08-20 09:14:24 +0100

    Integration testing all passed

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,6 +5,51 @@
 
 
 # This is an example test, replace it with your own test.
-describe port(7000), do
-  it { should be_listening }
+describe port(80) do
+  it { should_not be_listening }
+end
+
+describe pip('Flask') do
+  it { should be_installed }
+  its('version') { should eq '0.10.1' }
+end
+
+describe pip('Jinja2') do
+  it { should be_installed }
+  its('version') { should eq '2.7.3' }
+end
+
+describe pip('MarkupSafe') do
+  it { should be_installed }
+  its('version') { should eq '0.23' }
+end
+
+describe pip('Werkzeug') do
+  it { should be_installed }
+  its('version') { should eq '0.9.6' }
+end
+
+describe pip('Gnureadline') do
+  it { should be_installed }
+  its('version') { should eq '6.3.3' }
+end
+
+describe pip('Itsdangerous') do
+  it { should be_installed }
+  its('version') { should eq '0.24' }
+end
+
+describe pip('Rauth') do
+  it { should be_installed }
+  its('version') { should eq '0.7.0' }
+end
+
+describe pip('Requests') do
+  it { should be_installed }
+  its('version') { should eq '2.3.0' }
+end
+
+describe pip('Wsgiref') do
+  it { should be_installed }
+  its('version') { should eq '0.1.2' }
 end
